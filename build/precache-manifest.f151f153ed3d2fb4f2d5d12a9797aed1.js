1b27b1ed5d72dcbed7a828b318b7b2ae814c9c38
mista78 <m.karim78@icloud.com>
2019-08-12 23:54:02 +0200

    push

---
+++ build/precache-manifest.f151f153ed3d2fb4f2d5d12a9797aed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a9b45777445eed7b87896b3562a7e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "163388747c0e0181396e",
+    "url": "/static/css/2.9ee81b7b.chunk.css"
+  },
+  {
+    "revision": "fbda00e33b14560bd985",
+    "url": "/static/css/main.67e9194e.chunk.css"
+  },
+  {
+    "revision": "163388747c0e0181396e",
+    "url": "/static/js/2.f1c6d245.chunk.js"
+  },
+  {
+    "revision": "fbda00e33b14560bd985",
+    "url": "/static/js/main.89f4dbf5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
